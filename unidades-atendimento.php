986deac4b53521bf2db599d314b56d50e0854e8e
Marcos Cordeiro <marcosc974@gmail.com>
2026-01-13 16:24:22 -0300

    fix: styles

--- unidades-atendimento.php
+++ unidades-atendimento.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Unidades de Atendimento
  * Description: CRUD de Unidades de Atendimento com shortcode para exibição em cards filtráveis.
- * Version: 1.2.1
+ * Version: 1.2.2
  * Author: Marcos Cordeiro
  * Author URI:        https://github.com/marcoscti
  * License:           GPL-2.0+
@@ -69,8 +69,8 @@ add_action('save_post', function ($post_id) {
 });
 
 add_shortcode('unidades_atendimento', function () {
-    wp_enqueue_style('ua-style', UA_PLUGIN_URL . 'assets/style.css', [], "1.2.1", "all");
-    wp_enqueue_script('ua-script', UA_PLUGIN_URL . 'assets/script.js', [], "1.2.1", "all");
+    wp_enqueue_style('ua-style', UA_PLUGIN_URL . 'assets/style.css', [], "1.2.2", "all");
+    wp_enqueue_script('ua-script', UA_PLUGIN_URL . 'assets/script.js', [], "1.2.2", "all");
 
     $q = new WP_Query([
         'post_type' => 'unidade_atendimento',
@@ -91,7 +91,7 @@ add_shortcode('unidades_atendimento', function () {
             </div>
         </div>
         <div class="ua-count-itens">
-            <input type="search" id="ua-search-input" placeholder="Encontre uma unidade de Atendimento por nome ou endereço...">
+            <input type="search" id="ua-search-input" placeholder="Busque por nome ou endereço...">
         </div>
     </div>
     <div class="ua-cards">
